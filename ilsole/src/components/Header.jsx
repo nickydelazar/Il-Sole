0662293775d7fea931ad76df45616474508a0f11
PerezzL <116911307+PerezzL@users.noreply.github.com>
2024-10-15 14:47:03 -0300

    Update SignIn

--- ilsole/src/components/Header.jsx
+++ ilsole/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Box>
         <Flex bg="#660033" p='1.5%' alignItems="center" justifyContent="space-between">
-            <Box flex="1" display="flex" justifyContent="center" ml="10.5%">
+            <Box flex="1" display="flex" justifyContent="center" ml="10.3%">
               <Link to="/">
                 <Box as="img" src={logo} alt="Logo" boxSize="55px" h="100%" />
               </Link>
